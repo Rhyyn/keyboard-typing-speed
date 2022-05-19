e9b09d90a5dec2d98ec238557d64ae73529dc2ac
Rhyn <rhyyn.lol@gmail.com>
2022-05-19 14:57:29 +0200

    Fix: Better handler for events / implemented refs

--- src/Components/keyboard.tsx
+++ src/Components/keyboard.tsx
@@ -1,38 +1,56 @@
 import keyList from '../keyList.json';
-import React, { useRef } from 'react';
-import words from "../words.json";
+import RandomWords from './randomWords';
+import React, { useEffect, useRef } from 'react';
+import Input from './input';
+
 
 const keyboard = () => {
 
+    let refs: any = useRef([]); // arrays of ref
+    let currentRefs: string[] = refs.current; // defining var for array of refs
+    const inputRef: any = useRef(); // ref of input
 
-    words.commonWords.forEach(element => {
-        console.log(element);
-        
-    });
-    
 
-    document.addEventListener("keydown", event => {
-        const keyPressed: string = event.key.toUpperCase();
-        const keyElement = document.getElementById(keyPressed);
-        
-        if (keyElement !== null) {
-            keyElement.classList.add("pressed")
-            document.addEventListener('keyup', () => {
-                keyElement.classList.remove("pressed")
-            })
-        } else {
-            return null;
-        }
-    })
+    const addRefs = (el: any) => {  // Function to push each span Element into the array of refs
+        currentRefs.push(el)
+    }
+
+    useEffect(() => {
+
+        window.addEventListener('keydown', event => {   // event listener on keydown
+            let keyPressed: string = event.key.toUpperCase(); // get key user pressed and set it to UpperCase
+            if (keyPressed !== null) {
+                refs.current.map((item: any, index: number) => { // map through refs and find the element that match user key
+                    if (item.id === keyPressed) {
+                        item.classList.add('pressed') // add class pressed for visual feedback to the user
+                    }
+                });
+            }
+        });
+
+        window.addEventListener('keyup', event => { // event listener on keyup
+            let keyPressed: string = event.key.toUpperCase(); // get key user pressed and set it to UpperCase
+            if (keyPressed !== null) {
+                refs.current.map((item: any, index: number) => { // map through refs and find the element that match user key
+                    if (item.id === keyPressed) {
+                        item.classList.remove('pressed') // remove class pressed for visual feedback to the user
+                    }
+                });
+            }
+        })
+    }, [])
 
 
     return (
         <div className='keyboardContainer'>
-            <textarea></textarea>
-            {keyList.keys.map((keyRow: Array<string>, index: number) => (
+            <RandomWords></RandomWords>
+            <Input ref={inputRef}></Input>
+            {keyList.keys.map((keyRow: Array<string>, index: number) => ( // map through json 
                 <div className='row' key={index}>
-                    {keyRow.map((keyId: string, index: number) => (
-                        <div className={`key ${keyId}`} id={keyId.toUpperCase()} key={keyId + index} ><span>{keyId}</span></div>
+                    {keyRow.map((keyId: string, index: number) => ( // map through each key in the json 
+                        <div ref={addRefs} className={`key ${keyId}`} id={keyId.toUpperCase()} key={keyId + index} >
+                            <span className='word'>{keyId}</span>
+                        </div>
                     ))}
                 </div>
             ))

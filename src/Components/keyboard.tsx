b7b4d851981a8d1f4c55e6119d908d80cc39b1f6
Tony Migeon <tonymigeonpro@gmail.com>
2023-02-02 17:07:05 +0100

    Fix: <span> of words now stored in an array of ref

--- src/Components/keyboard.tsx
+++ src/Components/keyboard.tsx
@@ -1,28 +1,38 @@
 import keyList from "../keyList.json";
 import RandomWords from "./randomWords";
-import React, { useEffect, useRef } from "react";
+import React, { RefObject, useEffect, useRef } from "react";
 import Input from "./input";
 
 const keyboard = () => {
-    let refs: any = useRef([]); // arrays of ref
-    let currentRefs: string[] = refs.current; // defining var for array of refs
+    let keysRefs = useRef<HTMLDivElement[]>([]);
+    // let refs: any = useRef([]); // arrays of ref
+    // let currentRefs: string[] = refs.current; // defining var for array of refs
     const addRefs = (el: any) => {
         // Function to push each span Element into the array of refs
-        currentRefs.push(el);
+        keysRefs.current.push(el);
     };
 
-    const handleRef = React.useRef<HTMLInputElement>(null);
+    // create an array of Refs that is passed down to randomWords.tsx where we store each words so we can compare the user input vs the word and change the word's color
+    let handleRandomWordsRef = useRef<HTMLSpanElement[]>([]);
+    const addWordsRefs = (el: HTMLSpanElement) => {
+        // Function to push each span Element into the array of refs
+        handleRandomWordsRef.current = [...handleRandomWordsRef.current, el];
+    };
+
+    // create a ref that is passed down to input.tsx to store the input of the user so we compare it to the current selected word of the list handleRandomWordsRef
+    const handleInputRef = React.useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         window.addEventListener("keydown", (event) => {
             // event listener on keydown
             let keyPressed: string = event.key.toUpperCase(); // get key user pressed and set it to UpperCase
             if (keyPressed !== null) {
-                refs.current &&
-                    refs.current.map((item: any, index: number) => {
+                keysRefs.current &&
+                    keysRefs.current.map((item: any, index: number) => {
                         // map through refs and find the element that match user key
                         if (item.id && item.id === keyPressed) {
                             item.classList.add("pressed"); // add class pressed for visual feedback to the user
+                            //.toggle
                         }
                     });
             }
@@ -30,15 +40,16 @@ const keyboard = () => {
 
         window.addEventListener("keyup", (event) => {
             // event listener on keyup
-            console.log(handleRef?.current?.value);
+            console.log(handleInputRef?.current?.value);
             let keyPressed: string = event.key.toUpperCase(); // get key user pressed and set it to UpperCase
             if (keyPressed !== null) {
-                refs.current &&
-                    refs.current.map((item: any, index: number) => {
+                keysRefs.current &&
+                    keysRefs.current.map((item: any, index: number) => {
                         // map through refs and find the element that match user key
                         if (item.id && item.id === keyPressed) {
                             // console.log(item.classList);
                             item.classList.remove("pressed"); // remove class pressed for visual feedback to the user
+                            console.log(keysRefs.current);
                         }
                     });
             }
@@ -47,8 +58,8 @@ const keyboard = () => {
 
     return (
         <div className="keyboardContainer">
-            <RandomWords></RandomWords>
-            <Input ref={handleRef}></Input>
+            <RandomWords addWordsRef={addWordsRefs}></RandomWords>
+            <Input ref={handleInputRef}></Input>
             {keyList.keys.map(
                 (
                     keyRow: Array<string>,
--- src/Components/randomWords.tsx
+++ src/Components/randomWords.tsx
@@ -1,41 +1,47 @@
-import { useRef } from 'react';
-import words from '../words.json';
+import React from "react";
+import { useRef } from "react";
+import words from "../words.json";
 
-
-const randomWords = () => {
+const randomWords = (props: any) => {
     let randomWords: any[] = []; // define aray for random picked words
-    let refs = useRef([]); // define refs
-    let currentRefs: string[] = refs.current; // define array for refs
-    // console.log(refs.current);
+    let refs = useRef([] as Element[]); // define refs
 
-
-    function getRandom(arr:Array<string>, n:number) { // get a random number (n) of items in a given array (arr)
-        var result:string[] = new Array(n),
+    function getRandom(arr: Array<string>, n: number) {
+        // get a random number (n) of items in a given array (arr)
+        var result: string[] = new Array(n),
             len = arr.length,
             taken = new Array(len);
         if (n > len)
-            throw new RangeError("getRandom: more elements taken than available");
+            throw new RangeError(
+                "getRandom: more elements taken than available"
+            );
         while (n--) {
             var x = Math.floor(Math.random() * len);
             result[n] = arr[x in taken ? taken[x] : x];
             taken[x] = --len in taken ? taken[len] : len;
         }
-        randomWords.push(result) // push the result to the new Array
+        randomWords.push(result); // push the result to the new Array
         return result;
-    }   
-        getRandom(words.commonWords, 100)
-
-    const addRefs = (el: any) => { // function to add refs to the array
-        currentRefs.push(el)
     }
+    getRandom(words.commonWords, 100);
+
 
     return (
-        <div className='randomWordsContainer'>
-                {randomWords[0].map((item:string, index:number) => { // map through the random selected words
-                    return <span ref={addRefs} className={`word ${item}`} key={index}>{item}</span>
-                })}
+        <div className="randomWordsContainer">
+            {randomWords[0].map((item: string, index: number) => {
+                // map through the random selected words
+                return (
+                    <span
+                        ref={el => props.addWordsRef(el)}
+                        className={`word ${item}`}
+                        key={index}
+                    >
+                        {item}
+                    </span>
+                );
+            })}
         </div>
-    )
-}
+    );
+};
 
 export default randomWords;
